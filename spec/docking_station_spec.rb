3001bb9595f2b38d531b0ecac0e3b7846706cb79
Stefan Lissia <stefan.emailacct@gmail.com>
2015-04-30 17:39:33 +0100

    stage5 station capacity is 20 now

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -17,5 +17,13 @@ describe DockingStation do
 
   end
 
+  describe 'dock' do
+    it 'raises an error when full' do
+      20.times { subject.dock Bike.new }
+      expect { subject.dock Bike.new}.to raise_error 'Docking Station full'
+    end
+  end
+
+
 
 end
